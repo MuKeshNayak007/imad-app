747e6d07214dbef55e4b29656caa11ad88273af5
MuKesh Nayak <30654027+MuKeshNayak007@users.noreply.github.com>
2017-08-02 19:07:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,50 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleone = {
+    title: "Article One | MuKesh Nayak",
+    heading: "Creating First HTML File",
+    date:"August 2, 2017",
+    content: `
+            <p>This is the content of this first article. This is the content of this first article. This is the content of this first article. This is the content of this first article. This is the content of this first article. This is the content of this first article. This is the content of this first article. This is the content of this first article. This is the content of this first article. This is the content of this first article. This is the content of this first article. This is the content of this first article. This is the content of this first article. This is the content of this first article. This is the content of this first article. This is the content of this first article. 
+            </p>
+            
+            <p>This is the content of this first article. This is the content of this first article. This is the content of this first article. This is the content of this first article. This is the content of this first article. This is the content of this first article. This is the content of this first article. This is the content of this first article. This is the content of this first article. This is the content of this first article. This is the content of this first article. This is the content of this first article. This is the content of this first article. This is the content of this first article. This is the content of this first article. This is the content of this first article. 
+            </p>
+            
+            <p>This is the content of this first article. This is the content of this first article. This is the content of this first article. This is the content of this first article. This is the content of this first article. This is the content of this first article. This is the content of this first article. This is the content of this first article. This is the content of this first article. This is the content of this first article. This is the content of this first article. This is the content of this first article. This is the content of this first article. This is the content of this first article. This is the content of this first article. This is the content of this first article. 
+            </p>`
+    
+};
+
+var htmltemplate = 
+    `<html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+                <link href="/ui/style.css" rel="stylesheet" />
+
+     </head>
+     
+    <body>
+        <div class="container">
+        <div>
+            <a href="/">Home</a>
+        </div>
+        <hr/>
+        <h3> ${heading}</h3>
+        <div>
+            ${date}
+        </div>
+        <div>
+         ${content}
+        </div>
+        </div>
+    </body>
+
+</html>`;
 
 
 app.get('/', function (req, res) {
