0074dc62de04e212b1486eb9cbe903aa3dee967e
MuKesh Nayak <30654027+MuKeshNayak007@users.noreply.github.com>
2017-08-02 18:31:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,15 +15,13 @@ app.get('/article-one.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
-app.get('/article-two', function (req, res) {
-  res.send('Article Two is requested and will be served here.');
+app.get('/article-second.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-second.html'));
 });
 
-
-app.get('/article-three', function (req, res) {
-  res.send('Article three is requested and will be served here.');
+app.get('/article-three.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
-
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
